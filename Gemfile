dd15d6a0302a55642d9c557e8b073fbceda19890
Juhyeong Yu <juhyeong@sporkin.it>
2012-12-12 20:31:48 -0800

    rails project import

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rake', '10.0.2'
+gem 'rails', '3.2.0'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'jbuilder'
+
+group :test, :development do
+  gem 'spork'
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UiFront::Application.config.secret_token = '0b182bf616d507f142c188202c35232cc4dd6d762035a239731d79cae7e873618a88aff65c36d22774ffb7de8424c5e8acc097110e18d6e9d0bbf388c8e41e1b'
